c2972218f1819b02cc1efad9782634f326755c86
NagendraSatyaPrasad <150710315+NagendraSatyaPrasad@users.noreply.github.com>
2026-04-07 16:34:58 +0530

    Implement quiz client to fetch and display questions

---
+++ Day-4/(Q4)question_client.py
@@ -0,0 +1,33 @@
+import requests
+
+URL = "http://127.0.0.1:5000/get_questions"
+
+def start_quiz():
+    res = requests.get(URL)
+    data = res.json()
+
+    score = 0
+
+    for i, q in enumerate(data, start=1):
+        print(f"\n{i}) {q['question']}")
+        print(f"a) {q['op1']}")
+        print(f"b) {q['op2']}")
+        print(f"c) {q['op3']}")
+        print(f"d) {q['op4']}")
+
+        ans = input("Enter your option (a/b/c/d): ")
+
+        mapping = {
+            "a": "op1",
+            "b": "op2",
+            "c": "op3",
+            "d": "op4"
+        }
+
+        if mapping.get(ans) == q['correct']:
+            score += 1
+
+    print(f"\nMarks = {score}/{len(data)}")
+
+if __name__ == "__main__":
+    start_quiz()
